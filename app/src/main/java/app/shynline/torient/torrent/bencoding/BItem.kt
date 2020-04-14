aad9168c67e671cb7c17ae9f3c7f75d622c8f947
Shayan <shayansabahi94@gmail.com>
2020-04-15 01:05:00 +0430

    Add BItem abstract class

---
+++ app/src/main/java/app/shynline/torient/torrent/bencoding/BItem.kt
@@ -0,0 +1,19 @@
+package app.shynline.torient.torrent.bencoding
+
+abstract class BItem<TYPE>(bencoded: String?, item: TYPE?) {
+    private var data: TYPE? = null
+
+    init {
+        assertJustOneSet("initialize", item, bencoded)
+        initialize(bencoded, item)
+    }
+
+    private fun initialize(bencoded: String?, item: TYPE?) {
+        data = item ?: decode(bencoded!!)
+    }
+
+    fun value(): TYPE = data!!
+    abstract fun encode(): String
+    protected abstract fun decode(bencoded: String): TYPE
+
+}
---
+++ app/src/main/java/app/shynline/torient/torrent/bencoding/Common.kt
@@ -0,0 +1,14 @@
+package app.shynline.torient.torrent.bencoding
+
+fun assertJustOneSet(
+    method: String,
+    b: Any?,
+    a: String?
+) {
+    if (a == null && b == null) {
+        throw IllegalArgumentException("$method: You must specify a String or an object")
+    }
+    if (a != null && b != null) {
+        throw IllegalArgumentException("$method: You must specify either String or an object")
+    }
+}
