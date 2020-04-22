994eb12292b69682fcdbae5ce411c9cecf0bd0cb
Shayan <shayansabahi94@gmail.com>
2020-04-22 21:27:03 +0430

    Add Koin to application

---
+++ app/src/main/java/app/shynline/torient/BaseApplication.kt
@@ -0,0 +1,18 @@
+package app.shynline.torient
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+
+class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(this@BaseApplication)
+        }
+    }
+}
