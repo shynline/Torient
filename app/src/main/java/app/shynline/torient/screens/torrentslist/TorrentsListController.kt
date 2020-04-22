af212a30f0f949121c4c87ab5311e0a23672b874
Shayan <shayansabahi94@gmail.com>
2020-04-22 21:24:32 +0430

    Implement the basic functionality of TorrentListFragment and controller

--- app/src/main/java/app/shynline/torient/screens/torrentslist/TorrentsListController.kt
+++ app/src/main/java/app/shynline/torient/screens/torrentslist/TorrentsListController.kt
@@ -1,4 +1,19 @@
 package app.shynline.torient.screens.torrentslist
 
 class TorrentsListController {
+
+    private lateinit var viewMvc: TorrentListViewMvc
+
+
+    fun bind(viewMvc: TorrentListViewMvc) {
+        this.viewMvc = viewMvc
+    }
+
+    fun onStart() {
+
+    }
+
+    fun onStop() {
+
+    }
 }
--- app/src/main/java/app/shynline/torient/screens/torrentslist/TorrentsListFragment.kt
+++ app/src/main/java/app/shynline/torient/screens/torrentslist/TorrentsListFragment.kt
@@ -1,7 +1,37 @@
 package app.shynline.torient.screens.torrentslist
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import app.shynline.torient.common.di.viewfactory.ViewMvcFactory
 import app.shynline.torient.screens.common.BaseFragment
+import org.koin.android.ext.android.inject
+import org.koin.androidx.scope.lifecycleScope
 
 class TorrentsListFragment : BaseFragment() {
+
+    private val viewMvcFactory by inject<ViewMvcFactory>()
+    private val controller by lifecycleScope.inject<TorrentsListController>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val viewMvc = viewMvcFactory.getTorrentListViewMvc(inflater, container)
+        controller.bind(viewMvc)
+        return viewMvc.getRootView()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        controller.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        controller.onStop()
+    }
 
 }
