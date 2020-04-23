3a6c5b53326aa95c92232d3ebb36072f82823ceb
Shayan <shayansabahi94@gmail.com>
2020-04-23 17:52:19 +0430

    Add a temporary button to TorrentListFragment

--- app/src/main/java/app/shynline/torient/screens/torrentslist/TorrentListViewMvcImpl.kt
+++ app/src/main/java/app/shynline/torient/screens/torrentslist/TorrentListViewMvcImpl.kt
@@ -2,6 +2,7 @@ package app.shynline.torient.screens.torrentslist
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Button
 import app.shynline.torient.R
 import app.shynline.torient.screens.common.view.BaseObservableViewMvc
 
@@ -10,8 +11,15 @@ class TorrentListViewMvcImpl(
     parent: ViewGroup?
 ) : BaseObservableViewMvc<TorrentListViewMvc.Listener>(), TorrentListViewMvc {
 
+    private val addTorrentFileBtn: Button
     init {
         inflater.inflate(R.layout.fragment_torrent_list_view, parent, false)
+        addTorrentFileBtn = findViewById(R.id.addTorrentFile)
+        addTorrentFileBtn.setOnClickListener {
+            getListeners().forEach {
+                it.addTorrentFile()
+            }
+        }
     }
 
 }
