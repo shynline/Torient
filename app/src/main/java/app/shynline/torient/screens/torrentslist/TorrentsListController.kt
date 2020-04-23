bb2522dc5ad5c080193d1c13e21634f2a2f5a727
Shayan <shayansabahi94@gmail.com>
2020-04-23 17:53:24 +0430

    Implement opening a torrent file by user

--- app/src/main/java/app/shynline/torient/screens/torrentslist/TorrentsListController.kt
+++ app/src/main/java/app/shynline/torient/screens/torrentslist/TorrentsListController.kt
@@ -1,19 +1,33 @@
 package app.shynline.torient.screens.torrentslist
 
-class TorrentsListController {
+import app.shynline.torient.screens.common.requesthelper.FragmentRequestHelper
+
+class TorrentsListController : TorrentListViewMvc.Listener {
 
     private lateinit var viewMvc: TorrentListViewMvc
+    private lateinit var fragmentRequestHelper: FragmentRequestHelper
 
 
-    fun bind(viewMvc: TorrentListViewMvc) {
+    fun bind(viewMvc: TorrentListViewMvc, fragmentRequestHelper: FragmentRequestHelper) {
         this.viewMvc = viewMvc
+        this.fragmentRequestHelper = fragmentRequestHelper
     }
 
     fun onStart() {
-
+        viewMvc.registerListener(this)
     }
 
     fun onStop() {
+        viewMvc.unRegisterListener(this)
+    }
+
+    override fun addTorrentFile() {
+        fragmentRequestHelper.openTorrentFile(REQUEST_ID_OPEN_TORRENT_FILE)
+    }
+
+    fun openTorrentFile(torrentData: ByteArray) {
 
     }
+
+
 }
--- app/src/main/java/app/shynline/torient/screens/torrentslist/TorrentsListFragment.kt
+++ app/src/main/java/app/shynline/torient/screens/torrentslist/TorrentsListFragment.kt
@@ -1,13 +1,19 @@
 package app.shynline.torient.screens.torrentslist
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import app.shynline.torient.common.di.viewfactory.ViewMvcFactory
 import app.shynline.torient.screens.common.BaseFragment
+import app.shynline.torient.screens.common.requesthelper.FragmentRequestHelperImpl
 import org.koin.android.ext.android.inject
 import org.koin.androidx.scope.lifecycleScope
+import java.io.BufferedInputStream
+
+const val REQUEST_ID_OPEN_TORRENT_FILE = 100
 
 class TorrentsListFragment : BaseFragment() {
 
@@ -20,7 +26,12 @@ class TorrentsListFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View? {
         val viewMvc = viewMvcFactory.getTorrentListViewMvc(inflater, container)
-        controller.bind(viewMvc)
+        controller.bind(
+            viewMvc,
+            FragmentRequestHelperImpl(
+                this
+            )
+        )
         return viewMvc.getRootView()
     }
 
@@ -32,6 +43,22 @@ class TorrentsListFragment : BaseFragment() {
     override fun onStop() {
         super.onStop()
         controller.onStop()
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_ID_OPEN_TORRENT_FILE && resultCode == Activity.RESULT_OK) {
+            // The result data contains a URI for the document or directory that
+            // the user selected.
+            data?.data?.also { uri ->
+                requireContext().contentResolver.openInputStream(uri)?.use {
+                    BufferedInputStream(it).use { s ->
+                        controller.openTorrentFile(s.readBytes())
+                    }
+                }
+            }
+
+        }
     }
 
 }
