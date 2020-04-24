48467ffed14896602f3c07225787eebeed4c7358
Shayan <shayansabahi94@gmail.com>
2020-04-24 16:10:25 +0430

    Instantiate SessionManager in Service and add start/stop interface

---
+++ app/src/main/java/app/shynline/torient/torrent/service/ITorrentService.kt
@@ -0,0 +1,6 @@
+package app.shynline.torient.torrent.service
+
+interface ITorrentService {
+    fun startSession()
+    fun stopSession()
+}
--- app/src/main/java/app/shynline/torient/torrent/service/TorientService.kt
+++ app/src/main/java/app/shynline/torient/torrent/service/TorientService.kt
@@ -3,19 +3,49 @@ package app.shynline.torient.torrent.service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
+import com.frostwire.jlibtorrent.AlertListener
+import com.frostwire.jlibtorrent.SessionManager
+import com.frostwire.jlibtorrent.alerts.AddTorrentAlert
+import com.frostwire.jlibtorrent.alerts.Alert
+import com.frostwire.jlibtorrent.alerts.AlertType
 
-class TorientService : ObservableService<TorientService.Listener>() {
+class TorientService : ObservableService<TorientService.Listener>(), ITorrentService,
+    AlertListener {
 
     interface Listener {
 
     }
 
+    private lateinit var session: SessionManager
+
+    override fun alert(p0: Alert<*>?) {
+        val type = p0?.type()
+        when (type) {
+            AlertType.ADD_TORRENT -> {
+                (p0 as? AddTorrentAlert)?.handle()?.resume()
+            }
+            AlertType.BLOCK_FINISHED -> {
+
+            }
+            AlertType.TORRENT_FINISHED -> {
+
+            }
+        }
+    }
+
+    override fun types(): IntArray {
+        return intArrayOf()
+    }
+
     override fun onCreate() {
         super.onCreate()
+        session = SessionManager()
+        session.addListener(this)
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        session.removeListener(this)
     }
 
 
@@ -29,4 +59,12 @@ class TorientService : ObservableService<TorientService.Listener>() {
 
 
     class TorientBinder(val service: TorientService) : Binder()
+
+    override fun startSession() {
+        session.start()
+    }
+
+    override fun stopSession() {
+        session.stop()
+    }
 }
