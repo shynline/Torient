844a3e658881bb5ab997eca1fd00f878a048c60a
Shayan <shayansabahi94@gmail.com>
2020-04-29 14:49:21 +0430

    Add a extension to context for providing a download directory file

---
+++ app/src/main/java/app/shynline/torient/common/ext.kt
@@ -0,0 +1,13 @@
+package app.shynline.torient.common
+
+import android.content.Context
+import app.shynline.torient.Config
+import java.io.File
+
+val Context.downloadDir: File
+    get() {
+        val file = File(filesDir, Config.baseDownloadDir)
+        if (!file.exists())
+            file.mkdir()
+        return file
+    }
