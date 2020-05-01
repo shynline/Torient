f7911b7fa4c25a0d2d39dd15d5fe87fef9a3d83a
Shayan <shayansabahi94@gmail.com>
2020-05-01 23:40:44 +0430

    Add an event class for routing torrent progress

--- app/src/main/java/app/shynline/torient/torrent/TorrentEvent.kt
+++ app/src/main/java/app/shynline/torient/torrent/TorrentEvent.kt
@@ -1,4 +1,4 @@
-package app.shynline.torient.model
+package app.shynline.torient.torrent
 
 sealed class TorrentEvent(val infoHash: String)
 class AddTorrentEvent(infoHash: String, val succeed: Boolean) : TorrentEvent(infoHash)
@@ -6,3 +6,6 @@ class TorrentResumedEvent(infoHash: String) : TorrentEvent(infoHash)
 class TorrentPausedEvent(infoHash: String) : TorrentEvent(infoHash)
 class TorrentFinishedEvent(infoHash: String) : TorrentEvent(infoHash)
 class TorrentRemovedEvent(infoHash: String) : TorrentEvent(infoHash)
+class TorrentProgressEvent(
+    infoHash: String, val progress: Float, val downloadRate: Int, val uploadRate: Int
+) : TorrentEvent(infoHash)
