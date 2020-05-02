68351fde4ebe1ecab6be2df5782182ddf3f8b213
Shayan <shayansabahi94@gmail.com>
2020-05-02 22:00:25 +0430

    Remove TorrentPausedEvent

--- app/src/main/java/app/shynline/torient/torrent/TorrentEvent.kt
+++ app/src/main/java/app/shynline/torient/torrent/TorrentEvent.kt
@@ -3,7 +3,6 @@ package app.shynline.torient.torrent
 sealed class TorrentEvent(val infoHash: String)
 class AddTorrentEvent(infoHash: String, val succeed: Boolean) : TorrentEvent(infoHash)
 class TorrentResumedEvent(infoHash: String) : TorrentEvent(infoHash)
-class TorrentPausedEvent(infoHash: String) : TorrentEvent(infoHash)
 class TorrentFinishedEvent(infoHash: String) : TorrentEvent(infoHash)
 class TorrentRemovedEvent(infoHash: String) : TorrentEvent(infoHash)
 class TorrentProgressEvent(
