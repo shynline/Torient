8d48b0b9b2ac73edb41b2b132645c691a10d4644
Shayan <shayansabahi94@gmail.com>
2020-05-03 20:13:52 +0430

    Add a "state" parameter for TorrentProgressEvent

--- app/src/main/java/app/shynline/torient/torrent/TorrentEvent.kt
+++ app/src/main/java/app/shynline/torient/torrent/TorrentEvent.kt
@@ -1,10 +1,16 @@
 package app.shynline.torient.torrent
 
+import app.shynline.torient.torrent.torrent.TorrentDownloadingState
+
 sealed class TorrentEvent(val infoHash: String)
 class AddTorrentEvent(infoHash: String, val succeed: Boolean) : TorrentEvent(infoHash)
 class TorrentResumedEvent(infoHash: String) : TorrentEvent(infoHash)
 class TorrentFinishedEvent(infoHash: String) : TorrentEvent(infoHash)
 class TorrentRemovedEvent(infoHash: String) : TorrentEvent(infoHash)
 class TorrentProgressEvent(
-    infoHash: String, val progress: Float, val downloadRate: Int, val uploadRate: Int
+    infoHash: String,
+    val state: TorrentDownloadingState,
+    val progress: Float = 0f,
+    val downloadRate: Int = 0,
+    val uploadRate: Int = 0
 ) : TorrentEvent(infoHash)
