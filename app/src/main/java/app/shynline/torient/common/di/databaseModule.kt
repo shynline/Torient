fe6bf8b971e4212d30c146f44c70b462a390fcbf
Shayan <shayansabahi94@gmail.com>
2020-05-03 20:18:48 +0430

    Add dataSource injection to Koin module

--- app/src/main/java/app/shynline/torient/common/di/databaseModule.kt
+++ app/src/main/java/app/shynline/torient/common/di/databaseModule.kt
@@ -2,6 +2,8 @@ package app.shynline.torient.common.di
 
 import androidx.room.Room
 import app.shynline.torient.database.TorrentDataBase
+import app.shynline.torient.database.datasource.TorrentDataSource
+import app.shynline.torient.database.datasource.TorrentDataSourceImpl
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
@@ -12,5 +14,9 @@ val databaseModule = module {
 
     single {
         get<TorrentDataBase>().torrentDao()
+    }
+
+    single<TorrentDataSource> {
+        TorrentDataSourceImpl(get(), get())
     }
 }
