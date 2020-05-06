0fcbfd1bc64676ac6266020f79ce6ae07ec56dd2
Shayan <shayansabahi94@gmail.com>
2020-05-06 15:38:41 +0430

    Remove unnecessary precision for bytes in transferRateUtils

--- app/src/main/java/app/shynline/torient/utils/TransferRateUtils.kt
+++ app/src/main/java/app/shynline/torient/utils/TransferRateUtils.kt
@@ -1,11 +1,13 @@
 package app.shynline.torient.utils
 
 fun Long.toStandardRate(): String {
+    var byte = true
     var u = "B/s"
     var sr = this.toFloat()
     if (sr > 1024) {
         sr /= 1024
         u = "KB/s"
+        byte = false
     }
     if (sr > 1024) {
         sr /= 1024
@@ -15,7 +17,7 @@ fun Long.toStandardRate(): String {
         sr /= 1024
         u = "GB/s"
     }
-    return String.format("%.2f $u", sr)
+    return if (byte) String.format("%d $u", sr.toInt()) else String.format("%.2f $u", sr)
 }
 
 fun Int.toStandardRate(): String {
