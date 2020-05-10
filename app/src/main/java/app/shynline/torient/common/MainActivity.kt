0c6955d73ec214ca86bc3a7116a18f5217c7a8d7
Shayan <shayansabahi94@gmail.com>
2020-05-10 13:01:35 +0430

    Implement torrent file opens by intent

--- app/src/main/java/app/shynline/torient/common/MainActivity.kt
+++ app/src/main/java/app/shynline/torient/common/MainActivity.kt
@@ -9,8 +9,11 @@ import app.shynline.torient.torrent.mediator.TorrentMediator
 import app.shynline.torient.torrent.torrent.Torrent
 import app.shynline.torient.torrent.torrent.TorrentController
 import app.shynline.torient.torrent.utils.Magnet
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.get
 import org.koin.android.ext.android.inject
+import java.io.BufferedInputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,7 +24,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         torrentController = get<Torrent>() as TorrentController
+        handleIntent()
+    }
 
+    override fun onStart() {
+        super.onStart()
+        torrentController.onActivityStart()
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        torrentController.onActivityStop()
+    }
+
+    private fun handleIntent() {
+        if (intent.type == "application/x-bittorrent" ||
+            intent.type?.startsWith("text/") == true
+        ) { // torrent file handling
+
+            intent.data?.let { uri ->
+                GlobalScope.launch {
+                    contentResolver.openInputStream(uri)?.use { inputStream ->
+                        val data = BufferedInputStream(inputStream).readBytes()
+                        torrentMediator.getTorrentDetail(torrentFile = data)?.let { torrentDetail ->
+                            PageNavigationHelper(findNavController(R.id.nav_host_fragment))
+                                .showNewTorrentDialog(torrentDetail.infoHash)
+                        }
+                    }
+                }
+            }
+            return
+        }
+
+        // magnet link
         intent.dataString?.let {
             if (it.startsWith("magnet")) {
                 val magnet = Magnet.parse(it)
@@ -36,16 +72,5 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        torrentController.onActivityStart()
-    }
-
-
-    override fun onStop() {
-        super.onStop()
-        torrentController.onActivityStop()
     }
 }
