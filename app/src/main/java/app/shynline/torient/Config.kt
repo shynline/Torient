9c6cc5724f0e217f6e581c05142b95521cd395be
Shayan <shayansabahi94@gmail.com>
2020-05-13 16:08:16 +0430

    Add a copy util for files with progress callback

--- app/src/main/java/app/shynline/torient/Config.kt
+++ app/src/main/java/app/shynline/torient/Config.kt
@@ -3,4 +3,7 @@ package app.shynline.torient
 object Config {
     const val baseDownloadDir = "download"
     const val baseTorrentDir = "torrents"
+
+    const val DEFAULT_FILE_BUFFER_SIZE = 8 * 1024
+    const val DEFAULT_COPY_FILE_CHECKPOINT_SIZE = 512 * 1024
 }
--- app/src/main/java/app/shynline/torient/utils/FileUtils.kt
+++ app/src/main/java/app/shynline/torient/utils/FileUtils.kt
@@ -1,6 +1,9 @@
 package app.shynline.torient.utils
 
+import app.shynline.torient.Config
 import java.io.File
+import java.io.InputStream
+import java.io.OutputStream
 
 fun File.calculateTotalSize(): Long {
     if (this.isDirectory) {
@@ -11,4 +14,30 @@ fun File.calculateTotalSize(): Long {
         return s
     }
     return this.length()
+}
+
+fun InputStream.copyTo(
+    out: OutputStream,
+    bufferSize: Int = Config.DEFAULT_FILE_BUFFER_SIZE,
+    checkPoint: Int = Config.DEFAULT_COPY_FILE_CHECKPOINT_SIZE,
+    callback: (delta: Long, progress: Long) -> Unit
+): Long {
+    var bytesCopied: Long = 0
+    val buffer = ByteArray(bufferSize)
+    var currentCheckPoint = 0L
+    var bytes = read(buffer)
+    while (bytes >= 0) {
+        out.write(buffer, 0, bytes)
+        bytesCopied += bytes
+        currentCheckPoint += bytes
+        if (currentCheckPoint >= checkPoint) {
+            callback.invoke(currentCheckPoint, bytesCopied)
+            currentCheckPoint = 0
+        }
+        bytes = read(buffer)
+    }
+    if (currentCheckPoint > 0) {
+        callback.invoke(currentCheckPoint, bytesCopied)
+    }
+    return bytesCopied
 }
