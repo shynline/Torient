276986747900037997a030225a7cd21ae008d843
Shayan <shayansabahi94@gmail.com>
2020-05-14 13:44:08 +0430

    Fix ETA showing zero values for time

--- app/src/main/java/app/shynline/torient/utils/TimeUtils.kt
+++ app/src/main/java/app/shynline/torient/utils/TimeUtils.kt
@@ -17,8 +17,9 @@ fun Long.toReadableTime(): String {
     } else {
         val r = ((timeF - time) * 60).toLong()
         val seconds = if (r == 1L) "" else "s"
+        val second = if (r > 0) ", $r second$seconds" else ""
         val minutes = if (time == 1L) "" else "s"
-        return "$time minute$minutes, $r second$seconds remaining"
+        return "$time minute$minutes$second remaining"
     }
     if (timeF > 24) {
         timeF /= 24 // Convert to Day
@@ -26,15 +27,17 @@ fun Long.toReadableTime(): String {
     } else {
         val r = ((timeF - time) * 60).toLong()
         val minutes = if (r == 1L) "" else "s"
+        val minute = if (r > 0) ", $r minute$minutes" else ""
         val hours = if (time == 1L) "" else "s"
-        return "$time hour$hours, $r minute$minutes remaining"
+        return "$time hour$hours$minute remaining"
     }
     if (timeF > 365) {
         return "" // More than a year
     }
     val r = ((timeF - time) * 24).toLong()
     val hours = if (r == 1L) "" else "s"
+    val hour = if (r > 0) ", $r hour$hours" else ""
     val days = if (time == 1L) "" else "s"
-    return "$time day$days, $r hour$hours remaining"
+    return "$time day$days$hour remaining"
 
 }
