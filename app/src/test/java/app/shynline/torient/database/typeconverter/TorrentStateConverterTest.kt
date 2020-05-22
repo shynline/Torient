c2c30629c620adb6a365fb6d75f5c9e5e7293150
Shayan <shayansabahi94@gmail.com>
2020-05-22 21:43:07 +0430

    Implement test class for TorrentStateConverter

---
+++ app/src/test/java/app/shynline/torient/database/typeconverter/TorrentStateConverterTest.kt
@@ -0,0 +1,41 @@
+package app.shynline.torient.database.typeconverter
+
+import app.shynline.torient.database.states.TorrentUserState
+import com.google.common.truth.Truth
+import org.junit.Assert.fail
+import org.junit.Before
+import org.junit.Test
+
+class TorrentStateConverterTest {
+
+    private lateinit var SUT: TorrentStateConverter
+
+    @Before
+    fun setup() {
+        SUT = TorrentStateConverter()
+    }
+
+    @Test
+    fun test_toInteger_returnsTheIdOfEnum() {
+        val result = SUT.toInteger(TorrentUserState.PAUSED)
+        Truth.assertThat(result).isEqualTo(TorrentUserState.PAUSED.id)
+    }
+
+
+    @Test
+    fun test_toTorrentState_returnsTheEnumValue() {
+        val result = SUT.toTorrentState(TorrentUserState.PAUSED.id)
+        Truth.assertThat(result).isEqualTo(TorrentUserState.PAUSED)
+    }
+
+    @Test
+    fun test_toTorrentState_wrongIdThrowsException() {
+        try {
+            SUT.toTorrentState(666)
+            fail("Should have thrown NoSuchElement exception");
+        } catch (e: NoSuchElementException) {
+            // Succeed
+        }
+
+    }
+}
